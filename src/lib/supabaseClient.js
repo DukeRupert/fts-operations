28535b97b5e60708176fb0a697fb5d59dee56143
Logan Williams <logan@firefly.llc>
2022-08-02 20:37:11 -0600

    Fixed unreachable environment variables

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,12 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 import { dev } from '$app/env';
 
+let supabaseUrl;
+let supabaseAnonKey;
+
 if (dev) {
-	const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-	const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+	supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+	supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 } else {
-	const supabaseUrl = process.env.SUPABASE_URL;
-	const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+	supabaseUrl = process.env.SUPABASE_URL;
+	supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

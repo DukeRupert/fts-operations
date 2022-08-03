556b160d49348ff139467920b142e59984dc45b0
Logan Williams <logan@firefly.llc>
2022-08-02 20:28:39 -0600

    supabaseClient configured to use dev and deployment variables.

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
+import { dev } from '$app/env';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+if (dev) {
+	const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+	const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+} else {
+	const supabaseUrl = process.env.SUPABASE_URL;
+	const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
